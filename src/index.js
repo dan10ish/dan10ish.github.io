9e323cd5b4102311acad8e309cc1e994b23fecf3
Danish Ansari <aaansaridan@gmail.com>
2024-07-14 21:28:27 +0300

    Adding Slug functionality

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </Router>
 );
