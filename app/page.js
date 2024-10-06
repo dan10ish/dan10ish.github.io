fe59eec68646de2658f62502366adc3cbbaabc0c
dan10ish <aaansaridan@gmail.com>
2024-10-07 01:06:30 +0530

    📚 Added Library

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import { getBlogPosts } from "../lib/api";
 import BlogList from "../components/BlogList";
 import AboutMe from "../components/AboutMe";
 import SocialIcons from "../components/SocialIcons";
+import Library from "../components/Library";
 
 export default function Home() {
   const posts = getBlogPosts();
@@ -16,6 +17,7 @@ export default function Home() {
       <AboutMe />
       <SocialIcons />
       <BlogList posts={posts} />
+      <Library />
     </main>
   );
 }
---
+++ components/Library.js
@@ -0,0 +1,64 @@
+"use client";
+
+import React from "react";
+import { getBooks } from "../lib/api";
+
+const Library = () => {
+  const books = getBooks();
+
+  const getBookDimensions = (size) => {
+    switch (size) {
+      case "s":
+        return { height: "200px", width: "33px" };
+      case "m":
+        return { height: "240px", width: "38px" };
+      case "l":
+        return { height: "280px", width: "43px" };
+      default:
+        return { height: "240px", width: "38px" };
+    }
+  };
+
+  const formatAuthorName = (author) => {
+    const names = author.split(" ");
+    if (names.length > 1) {
+      return (
+        <>
+          <span>{names[0]}</span>
+          <span>{names.slice(1).join(" ")}</span>
+        </>
+      );
+    }
+    return author;
+  };
+
+  return (
+    <section className="library">
+      <h2>Library</h2>
+      <div className="book-shelf">
+        {books.map((book, index) => {
+          const { height, width } = getBookDimensions(book.size);
+          return (
+            <div key={index} className="book-container">
+              <div
+                className="book"
+                style={{
+                  backgroundColor: book.color,
+                  height: height,
+                  width: width,
+                }}
+              >
+                <div className="book-title">{book.title}</div>
+                <div className="book-author">
+                  {formatAuthorName(book.author)}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Library;
