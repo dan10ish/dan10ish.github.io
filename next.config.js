eb12b6225bcb7e969246f39588e4df66e6040202
dan10ish <aaansaridan@gmail.com>
2024-10-29 17:35:59 +0530

    🛠️ Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
