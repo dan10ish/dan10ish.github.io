148bc4afad1ee70050d4858ffac95a7d6e8141db
dan10ish <aaansaridan@gmail.com>
2024-11-05 17:43:49 +0530

    ✨ Updated socials

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -1,24 +1,14 @@
 import { memo } from "react";
-import { Github, Instagram, Mail, FileText } from "lucide-react";
+import { Github, FileText, Mail } from "lucide-react";
 
 const SocialIcons = memo(() => (
   <div className="social-icons">
     <a
-      href="https://x.com/dan10ish"
-      target="_blank"
-      rel="noopener noreferrer"
+      href="mailto:aaansaridan@gmail.com"
       className="social-icon"
-      aria-label="Twitter"
+      aria-label="Email"
     >
-      <svg
-        width="22"
-        height="22"
-        viewBox="0 0 22 25"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M11.3848 10.7287L11.4667 10.6714L11.3848 10.7287L12.275 12.0021L18.6059 21.0577C19.023 21.6542 18.5962 22.4733 17.8683 22.4733H17.259C16.9651 22.4733 16.6898 22.3299 16.5214 22.089L10.7564 13.8426L10.6744 13.8999L10.7564 13.8426L9.86614 12.5693L3.8825 4.0103C3.46544 3.41375 3.89224 2.59462 4.62011 2.59462H5.22938C5.52325 2.59462 5.79862 2.7381 5.967 2.97896L11.3848 10.7287ZM13.9718 10.2204L20.7782 2.30852C21.2537 1.75581 20.861 0.9 20.1319 0.9C19.8836 0.9 19.6476 1.00828 19.4856 1.19654L13.2829 8.40655C12.8978 8.85418 12.1933 8.81595 11.8589 8.32928L7.08193 1.37706C6.87674 1.07843 6.53765 0.9 6.17532 0.9H1.90043C1.01487 0.9 0.492315 1.89307 0.993826 2.62295L8.26781 13.2092C8.49916 13.5458 8.47472 13.9961 8.20829 14.3058L0.993884 22.6914C0.518369 23.2441 0.911055 24.1 1.64017 24.1C1.88851 24.1 2.12451 23.9917 2.28647 23.8035L8.89674 16.1196C9.28183 15.672 9.98637 15.7102 10.3208 16.1969L15.4235 23.623C15.6287 23.9216 15.9678 24.1 16.3301 24.1H20.605C21.4906 24.1 22.0132 23.1069 21.5117 22.3771L13.9123 11.317C13.681 10.9803 13.7054 10.53 13.9718 10.2204Z" />
-      </svg>
+      <Mail strokeWidth={1.5} />
     </a>
 
     <a
@@ -32,21 +22,57 @@ const SocialIcons = memo(() => (
     </a>
 
     <a
-      href="https://instagram.com/dan10ish"
+      href="https://linkedin.com/in/dan10ish"
       target="_blank"
       rel="noopener noreferrer"
       className="social-icon"
-      aria-label="Instagram"
+      aria-label="LinkedIn"
     >
-      <Instagram strokeWidth={1.5} />
+      <svg
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.2"
+      >
+        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452z" />
+      </svg>
     </a>
 
     <a
-      href="mailto:aaansaridan@gmail.com"
+      href="https://leetcode.com/u/dan10ish/"
+      target="_blank"
+      rel="noopener noreferrer"
       className="social-icon"
-      aria-label="Email"
+      aria-label="LeetCode"
     >
-      <Mail strokeWidth={1.5} />
+      <svg
+        width="22"
+        height="24"
+        viewBox="0 0 22 26"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.6098 18.136L12.6968 22.049C12.3967 22.3505 12.04 22.5897 11.6472 22.7529C11.2544 22.9161 10.8332 23.0001 10.4078 23.0001C9.98245 23.0001 9.56128 22.9161 9.16847 22.7529C8.77567 22.5897 8.41897 22.3505 8.11886 22.049L2.39638 16.3268C2.09494 16.0267 1.85575 15.67 1.69254 15.2772C1.52933 14.8843 1.44531 14.4632 1.44531 14.0378C1.44531 13.6124 1.52933 13.1913 1.69254 12.7984C1.85575 12.4056 2.09494 12.0489 2.39638 11.7488L8.11886 6.02628C8.41897 5.72484 8.77567 5.48564 9.16847 5.32243C9.56128 5.15921 9.98245 5.0752 10.4078 5.0752C10.8332 5.0752 11.2544 5.15921 11.6472 5.32243C12.04 5.48564 12.3967 5.72484 12.6968 6.02628L16.9995 10.3289"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M8.11865 6.02615L13.1999 1"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M8.58496 14.0264H20.7075"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
     </a>
 
     <a
