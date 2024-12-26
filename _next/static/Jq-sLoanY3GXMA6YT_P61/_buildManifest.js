42c18a1668e3f51012336f567e4bae561be33afb
dan10ish <iamdan10ish@gmail.com>
2024-12-26 16:21:22 +0000

    Deploying to gh-pages from @ dan10ish/dan10ish.github.io@edca151c941dbbb8eb189a14a0427c483324249a 🚀

---
+++ _next/static/Jq-sLoanY3GXMA6YT_P61/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-4885ab50c46a4cfb.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
