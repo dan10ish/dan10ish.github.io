25c438608663afa3166dbdb33176a488d655a9c3
dan10ish <aansaridan@gmail.com>
2025-01-23 21:16:48 +0300

    Improved info UI

--- components/Content.jsx
+++ components/Content.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useMemo } from "react";
 import { supabase } from "@/lib/supabase";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Globe, Github, ArrowUp, ArrowDown, X } from "lucide-react";
+import { Globe, Github, ArrowUp, ArrowDown, X, Mail } from "lucide-react";
 import { Suspense } from "react";
 import Footer from "@/components/Footer";
 import Image from "next/image";
@@ -45,15 +45,53 @@ const InfoPopup = () => {
               <X size={16} />
             </button>
             <div className="info-content">
-              <Image
-                src="icons/icon.png"
-                alt="Danish"
-                className="info-avatar"
-                width={80}
-                height={80}
-              />
+              <div className="info-content-profile">
+                <Image
+                  src="icons/icon.png"
+                  alt="Danish"
+                  className="info-avatar"
+                  width={80}
+                  height={80}
+                />
+                <div className="footer-socials">
+                  <a
+                    href="https://x.com/dan10ish"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="header-icon"
+                    aria-label="Visit my X profile"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                      fill="currentColor"
+                    >
+                      <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                    </svg>
+                  </a>
+                  <a
+                    href="https://github.com/dan10ish"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="header-icon header-icon-github"
+                    aria-label="Visit my GitHub profile"
+                  >
+                    <Github size={18} />
+                  </a>
+                  <a
+                    href="mailto:aansaridan@gmail.com"
+                    className="header-icon email-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Connect via email"
+                  >
+                    <Mail size={20} />
+                  </a>
+                </div>
+              </div>
               <p className="info-text">
-                Danish is a <strong>mechatronics</strong> engineer exploring <code>machine learning</code>, <code>robotics</code>, and{" "}
+                Danish is a <strong>mechatronics</strong> engineer exploring{" "}
+                <code>machine learning</code>, <code>robotics</code>, and{" "}
                 <code>finance</code>.
               </p>
             </div>
--- components/Footer.js
+++ components/Footer.js
@@ -244,16 +244,6 @@ const Footer = ({ blogSlug = null }) => {
           </div>
           <div className="footer-socials">
             <a
-              href="https://github.com/dan10ish"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="header-icon header-icon-github"
-              aria-label="Visit my GitHub profile"
-            >
-              <Github size={18} />
-            </a>
-
-            <a
               href="https://x.com/dan10ish"
               target="_blank"
               rel="noopener noreferrer"
@@ -267,6 +257,15 @@ const Footer = ({ blogSlug = null }) => {
               >
                 <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
               </svg>
+            </a>
+            <a
+              href="https://github.com/dan10ish"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="header-icon header-icon-github"
+              aria-label="Visit my GitHub profile"
+            >
+              <Github size={18} />
             </a>
             <a
               href="mailto:aansaridan@gmail.com"
