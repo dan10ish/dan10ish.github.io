e9ce129e97feb00872d15ccf2806aef905f3bf89
dan10ish <aansaridan@gmail.com>
2025-01-27 09:08:22 +0300

    Made errors pages uniform

--- app/not-found.js
+++ app/not-found.js
@@ -1,3 +1,5 @@
+import { Github } from "lucide-react";
+
 export default function NotFound() {
   return (
     <div className="error-container">
@@ -12,14 +14,20 @@ export default function NotFound() {
 
       <div className="error-nav">
         <a href="/" className="return-link">
-          Home
+          Return to Homepage
         </a>
         <div className="social-links">
           <a href="https://github.com/dan10ish" className="social-link">
-            GitHub
+            <Github size={24} />
           </a>
-          <a href="https://x.com/dan10ish" className="social-link">
-            Twitter
+          <a href="https://x.com/dan10ish" className="social-link error-x-link">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+            >
+              <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+            </svg>
           </a>
         </div>
       </div>
