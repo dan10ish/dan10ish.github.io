a0b0714abf079b27fb4369737c8f00a4983acc4f
dan10ish <aansaridan@gmail.com>
2025-03-13 15:53:20 +0300

    Deploy changes and performance improvements

--- next.config.js
+++ next.config.js
@@ -3,19 +3,22 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+  experimental: {
+    webpackBuildWorker: true
   },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname),
+      "@": path.resolve(__dirname)
     };
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
