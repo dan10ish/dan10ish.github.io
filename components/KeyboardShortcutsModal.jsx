0b91c1decfedf9e29d1c0e67ca73b7cb30416935
dan10ish <aansaridan@gmail.com>
2025-03-25 04:53:55 +0300

    Bug Fixes

--- components/KeyboardShortcutsModal.jsx
+++ components/KeyboardShortcutsModal.jsx
@@ -13,43 +13,48 @@ const KeyboardShortcutsModal = ({ isOpen, onClose }) => {
 
   return (
     <div className="keyboard-shortcuts-overlay">
-      <motion.div 
-        className="keyboard-shortcuts-modal" 
+      <motion.div
+        className="keyboard-shortcuts-modal"
         ref={modalRef}
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ type: "spring", stiffness: 500, damping: 30 }}
       >
         <div className="keyboard-shortcuts-header">
-          <h3>keyboard shortcuts</h3>
+          <h3>Keyboard Shortcuts</h3>
         </div>
         <div className="keyboard-shortcuts-content">
           <div className="shortcut-group">
             <div className="shortcut-row">
               <div className="arrow-keys-grid">
-                <ShortcutKey className="arrow-key up-key"><ArrowUp size={12} /></ShortcutKey>
+                <ShortcutKey className="arrow-key up-key">
+                  <ArrowUp size={12} />
+                </ShortcutKey>
                 <div className="arrow-key-bottom-row">
-                  <ShortcutKey className="arrow-key"><ArrowLeft size={12} /></ShortcutKey>
-                  <ShortcutKey className="arrow-key"><ArrowDown size={12} /></ShortcutKey>
-                  <ShortcutKey className="arrow-key"><ArrowRight size={12} /></ShortcutKey>
+                  <ShortcutKey className="arrow-key">
+                    <ArrowLeft size={12} />
+                  </ShortcutKey>
+                  <ShortcutKey className="arrow-key">
+                    <ArrowDown size={12} />
+                  </ShortcutKey>
+                  <ShortcutKey className="arrow-key">
+                    <ArrowRight size={12} />
+                  </ShortcutKey>
                 </div>
               </div>
-              <span className="shortcut-description">navigate projects & photos</span>
+              <span className="shortcut-description">
+                Navigate projects & photos
+              </span>
             </div>
-            
+
             <div className="shortcut-row">
               <ShortcutKey className="text-key">enter</ShortcutKey>
-              <span className="shortcut-description">open selected item</span>
+              <span className="shortcut-description">Open selected item</span>
             </div>
-            
+
             <div className="shortcut-row">
               <ShortcutKey className="text-key">esc</ShortcutKey>
-              <span className="shortcut-description">close or exit</span>
-            </div>
-            
-            <div className="shortcut-row">
-              <ShortcutKey className="text-key">tab</ShortcutKey>
-              <span className="shortcut-description">switch tabs</span>
+              <span className="shortcut-description">Close or exit</span>
             </div>
           </div>
         </div>
@@ -58,4 +63,4 @@ const KeyboardShortcutsModal = ({ isOpen, onClose }) => {
   );
 };
 
-export default KeyboardShortcutsModal; 
+export default KeyboardShortcutsModal;
