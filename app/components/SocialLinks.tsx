07371057b1c479a5f1ff54e5df967024dea083c5
dan10ish <aansaridan@gmail.com>
2025-05-12 17:08:27 +0530

    Bug Fixes

--- app/components/SocialLinks.tsx
+++ app/components/SocialLinks.tsx
@@ -26,21 +26,17 @@ XIcon.displayName = 'XIcon';
 const LinkedInIcon = memo((props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
+    width="22"
+    height="22"
+    viewBox="0 0 30 30"
+    fill="currentColor"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth="0"
     strokeLinecap="round"
     strokeLinejoin="round"
     {...props}
   >
-    <rect x="2" y="2" width="20" height="20" rx="2" />
-    <path d="M8 11v5" />
-    <path d="M8 8v.01" />
-    <path d="M12 16v-5" />
-    <path d="M16 16v-3a2 2 0 0 0-4 0" />
+    <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"/>
   </svg>
 ));
 
