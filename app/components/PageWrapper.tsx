7ceb291f94be3f8a264bedd074efb981fc3bfc81
Danish <aaansaridan@gmail.com>
2025-05-14 18:21:18 +0000

    Removed Framer motion

--- app/components/PageWrapper.tsx
+++ app/components/PageWrapper.tsx
@@ -1,23 +1,9 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { motion, AnimatePresence } from "framer-motion";
 
 export const PageWrapper = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
-  const pathname = usePathname();
-
-  return (
-    <AnimatePresence>
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.15, ease: "easeInOut" }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  );
-}; 
+  return <div>{children}</div>;
+};
