6a734334e85bc9753c81f4cb10d10cb81902652e
Danish Ansari <aansaridan@gmail.com>
2025-05-30 12:24:30 +0530

    Color sceheme update

--- app/components/ThemeColorUpdater.tsx
+++ app/components/ThemeColorUpdater.tsx
@@ -2,36 +2,18 @@
 
 import { useEffect } from 'react';
 
-const LIGHT_THEME_COLOR = '#F7F7F7'; 
-const DARK_THEME_COLOR = '#121212';
+const THEME_COLOR = '#bbb';
 
 export function ThemeColorUpdater() {
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    let metaThemeColor = document.querySelector('meta[name="theme-color"]');
 
-    const updateThemeColor = (matches: boolean) => {
-      let color = matches ? DARK_THEME_COLOR : LIGHT_THEME_COLOR;
-      let metaThemeColor = document.querySelector('meta[name="theme-color"]');
-
-      if (!metaThemeColor) {
-        metaThemeColor = document.createElement('meta');
-        metaThemeColor.setAttribute('name', 'theme-color');
-        document.head.appendChild(metaThemeColor);
-      }
-      metaThemeColor.setAttribute('content', color);
-    };
-
-    updateThemeColor(mediaQuery.matches);
-
-    const listener = (e: MediaQueryListEvent) => {
-      updateThemeColor(e.matches);
-    };
-
-    mediaQuery.addEventListener('change', listener);
-
-    return () => {
-      mediaQuery.removeEventListener('change', listener);
-    };
+    if (!metaThemeColor) {
+      metaThemeColor = document.createElement('meta');
+      metaThemeColor.setAttribute('name', 'theme-color');
+      document.head.appendChild(metaThemeColor);
+    }
+    metaThemeColor.setAttribute('content', THEME_COLOR);
   }, []);
 
   return null;
