a47e4b0390295b84c65555c85980fb8eae56437e
Danish Ansari <aansaridan@gmail.com>
2025-07-08 20:20:38 +0530

    Added SEO stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,70 @@ const sfMono = localFont({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://dan10ish.github.io"),
   title: "Danish Ansari",
-  description: "Mechatronics Engineer",
+  description:
+  "Danish is a mechatronics engineer exploring machine learning, robotics and finance. This is his personal website.",
   icons: {
     icon: '/icon.png',
   },
+  keywords: [
+    "Danish",
+    "ai",
+    "artificial intelligence",
+    "Danish Ansari",
+    "Danish Mumbai",
+    "mechatronics",
+    "robotics",
+    "machine learning",
+    "developer",
+    "engineer",
+    "portfolio",
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  alternates: { canonical: "https://dan10ish.github.io" },
+  twitter: {
+    card: "summary_large_image",
+    site: "@dan10ish",
+    creator: "@dan10ish",
+    title: "Danish",
+    description:
+      "Danish's personal website featuring his projects and insights in robotics and machine learning.",
+    images: ["https://i.ibb.co/vmBrhSd/OG.png"],
+  },
+  openGraph: {
+    type: "website",
+    siteName: "Danish",
+    title: "Danish",
+    description:
+      "Danish's personal website featuring his projects and insights in robotics and machine learning.",
+    url: "https://dan10ish.github.io",
+    images: [
+      {
+        url: "https://i.ibb.co/vmBrhSd/OG.png",
+        width: 1200,
+        height: 675,
+        type: "image/png",
+        alt: "Danish's Portfolio Preview",
+      },
+    ],
+  },
+  other: {
+    "profile:username": "dan10ish",
+    "profile:first_name": "Danish",
+  },
 };
-
-
 
 export default function RootLayout({
   children,
