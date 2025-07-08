0856a68e17c5e2ac6f6bce1decc20a17ee63e034
Danish Ansari <aansaridan@gmail.com>
2025-07-08 16:32:16 +0530

    Reset

--- app/page.js
+++
@@ -1,7 +0,0 @@
-import { getProjects } from "../lib/projects";
-import ContentWrapper from "../components/Content";
-
-export default function Home() {
-  const projects = getProjects();
-  return <ContentWrapper projects={projects} />;
-}
