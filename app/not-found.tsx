f27dbc2f788e75e3d10de06fc5a7e49c2ce3b9a2
Danish Ansari <aansaridan@gmail.com>
2025-07-18 01:48:31 +0530

    error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,24 +6,22 @@ export default function NotFound() {
   const socialLinks = data.contact.filter(contact => contact.name !== 'AANSARIDAN@GMAIL.COM');
 
   return (
-    <main className="font-sans h-[100dvh] overflow-hidden relative bg-[rgb(var(--background))] text-[rgb(var(--foreground))] transition-colors duration-300">
-      <div className="absolute inset-0 flex items-center justify-center -translate-y-12">
-        <div className="text-center space-y-2">
-          <div className="text-6xl font-bold">404</div>
-          <div className="text-sm">Page not found</div>
+    <main className="font-sans h-[100dvh] flex flex-col items-center justify-center bg-[rgb(var(--background))] text-[rgb(var(--foreground))] transition-colors duration-300 overflow-hidden">
+      <div className="flex flex-col items-center space-y-12 -translate-y-6">
+        <img src="/error-asset.gif" alt="Error asset" className="w-60 object-contain rounded-xl" />
+        <div className="flex flex-col items-center space-y-2">
+          <div className="text-2xl font-bold">404</div>
+          <div className="text-sm uppercase">Page not found</div>
         </div>
-      </div>
-      
-      <div className="absolute bottom-4 left-0 right-0 pb-16 text-center space-y-6">
+        <div className="flex flex-col items-center space-y-6">
         <Link
           href="/"
-          className="inline-flex items-center gap-2 px-6 py-2 border border-gray-300 dark:border-gray-700 hover:scale-110 hover:text-blue-600 dark:hover:text-blue-300 text-sm rounded-md"
+          className="inline-flex items-center gap-2 px-6 py-2 bg-[#eeeeee] dark:bg-[#222222] hover:scale-110 hover:text-blue-600 dark:hover:text-blue-300 text-sm rounded-md uppercase"
         >
           Homepage
           <ArrowUpRight size={14} />
         </Link>
-        
-        <div className="flex flex-wrap justify-center gap-1 sm:gap-4 text-sm px-4">
+        <div className="flex flex-wrap justify-center gap-1 sm:gap-4 text-sm px-4 uppercase">
           {socialLinks.map((link, index) => (
             <a
               key={index}
@@ -35,7 +33,8 @@ export default function NotFound() {
               {link.name}
               <ArrowUpRight size={14} className="opacity-0 group-hover:opacity-100 transition-opacity" />
             </a>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </main>
