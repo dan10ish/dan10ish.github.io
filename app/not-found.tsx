eb2291d56001ab7ef323ddfc6fb5de8323eaa4c2
Danish Ansari <aansaridan@gmail.com>
2025-07-22 13:03:44 +0530

    Fixed 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,18 @@
 "use client";
-import Link from 'next/link';
-import { ArrowUpRight } from 'lucide-react';
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
 
 export default function NotFound() {
   return (
     <main className="font-sans h-[100dvh] flex flex-col items-center justify-center bg-[rgb(var(--background))] text-[rgb(var(--foreground))] transition-colors duration-300 overflow-hidden">
       <div className="flex flex-col items-center space-y-16">
-        <div className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col items-center space-y-3">
           <div className="text-4xl font-bold">404</div>
-          <div className="text-sm font-light uppercase">Page not found</div>
+          <div className="text-md font-medium uppercase">Page not found</div>
         </div>
         <Link
           href="/"
-          className="inline-flex items-center gap-2 px-6 py-2 bg-[rgb(var(--secondary))] hover:scale-110 !text-sm rounded-md uppercase"
+          className="inline-flex items-center gap-1 px-4 py-2 bg-[rgb(var(--secondary))] hover:ring-1 hover:ring-gray-500 hover:!text-[rgb(var(--foreground))] hover:scale-105 !text-sm rounded-lg"
         >
           Homepage
           <ArrowUpRight size={14} />
@@ -20,4 +20,4 @@ export default function NotFound() {
       </div>
     </main>
   );
-} 
+}
