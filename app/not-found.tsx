12103283428e6d9f716a32afdd299f80141fd100
Danish Ansari <aansaridan@gmail.com>
2025-08-18 10:22:58 +0300

    Error Page UI

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import { ArrowLeft } from 'lucide-react';
+import SocialLinks from './components/SocialLinks';
+import { personalInfo } from './data';
 
 export default function NotFound() {
   return (
@@ -7,11 +9,20 @@ export default function NotFound() {
       <div className="text-center space-y-4">
         <h1 className="text-base font-bold">404</h1>
         <p className="text-base opacity-70">page not found</p>
-        
-        <Link href="/" className="inline-flex items-center mt-6 hover-underline" aria-label="Go back to home page">
-          <ArrowLeft size={16} className="mr-2" />
+
+        <Link href="/" className="inline-flex items-center gap-2 mt-6 bg-[var(--code-bg)] !px-2 !py-1 rounded-md !mb-6" aria-label="Go back to home page">
+          <ArrowLeft size={16} />
           <span>back to home</span>
         </Link>
+
+        <div className="flex justify-center">
+        <SocialLinks
+          github={personalInfo.socials.github}
+          email={personalInfo.socials.email}
+          x={personalInfo.socials.x}
+          instagram={personalInfo.socials.instagram}
+          linkedin={personalInfo.socials.linkedin}
+        /></div>
       </div>
     </div>
   );
