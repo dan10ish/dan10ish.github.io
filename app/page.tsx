89c2eda6868b1f27b5f2fe8230163749ebd81845
Danish Ansari <aansaridan@gmail.com>
2026-04-06 01:37:28 +0300

    Spacing improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import { RiInstagramFill, RiThreadsLine, RiTwitterXLine } from "@remixicon/react";
 import { TextMarquee } from "@/components/text-marquee";
 import HighlightedText from "@/components/highlighted-text";
@@ -7,7 +6,7 @@ import HighlightedText from "@/components/highlighted-text";
 export default function Page() {
   return (
     <div className="min-h-svh p-8 md:p-6 flex items-center justify-center">
-      <div className="max-w-[400px]">
+      <div className="max-w-[400px] flex flex-col gap-6 md:gap-10">
         <TextMarquee
           height={50}
           speed={1}
@@ -22,15 +21,14 @@ export default function Page() {
           <p className="font-medium">travel</p>
           <p className="font-medium">play&nbsp;piano</p>
           <p className="font-medium">take&nbsp;videos</p>
-          <p className="font-medium">take&nbsp;companies&nbsp;from&nbsp;0&nbsp;to&nbsp;1</p>
           <p className="font-medium">read</p>
+          <p className="font-medium">play&nbsp;basketball</p>
           <p className="font-medium">sleep</p>
         </TextMarquee>
-        <Separator className="my-4" />
         <div>
-          <p className="text-muted-foreground font-medium">I&apos;m a <HighlightedText from="left" delay={0.4}>mechatronics</HighlightedText> engineer integrating hardware and software to create applications in machine learning, robotics, and finance. Currently taking companies from <HighlightedText delay={0.8} from="top">0 to 1</HighlightedText>.</p>
+          <p className="text-muted-foreground font-medium">I&apos;m a <HighlightedText from="left" delay={0.4}>mechatronics</HighlightedText> engineer integrating hardware and software to create applications in machine learning, robotics, and finance. Currently taking companies from <HighlightedText delay={0.8} from="top">0 to 1</HighlightedText></p>
         </div>
-        <div className="flex gap-2 my-4">
+        <div className="flex gap-2">
           <Button variant="outline" size="sm"> <RiTwitterXLine /> dan10ish</Button>
           <Button variant="outline" size="sm"> <RiInstagramFill /> dan10ish</Button>
           <Button variant="outline" size="sm"> <RiThreadsLine /> dan10ish</Button>
