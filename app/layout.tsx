660db69e1a581b9d39dbb69a3ae58491b7252acb
Danish Ansari <aansaridan@gmail.com>
2026-04-06 01:38:39 +0300

    refactor: remove Geist Mono font and add site metadata to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
 
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+export const metadata = {
+  title: "Danish",
+  description: "Danish's Portfolio",
+}
 
 export default function RootLayout({
   children,
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn("antialiased", "font-sans", geist.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
