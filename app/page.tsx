f253de196e57002cd4b904fa59df93c60493e64e
Danish Ansari <aansaridan@gmail.com>
2026-04-06 13:27:01 +0300

    Try v3

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { RiInstagramFill, RiThreadsLine, RiTwitterXLine } from "@remixicon/react";
-import { TextMarquee } from "@/components/text-marquee";
 import HighlightedText from "@/components/highlighted-text";
-
 export default function Page() {
   return (
     <div className="min-h-svh p-8 md:p-6 flex items-center justify-center">
-      <div className="max-w-[400px] flex flex-col gap-6 md:gap-10">
-        <TextMarquee
-          height={50}
-          speed={1}
-          prefix={
-            <span>
-              <span className="font-semibold">Danish</span>, I&nbsp;
-            </span>
-          }
-        >
-          <p className="font-medium">code</p>
-          <p className="font-medium">play&nbsp;football</p>
-          <p className="font-medium">travel</p>
-          <p className="font-medium">play&nbsp;piano</p>
-          <p className="font-medium">take&nbsp;videos</p>
-          <p className="font-medium">read</p>
-          <p className="font-medium">play&nbsp;basketball</p>
-          <p className="font-medium">sleep</p>
-        </TextMarquee>
+      <div className="max-w-[400px] flex flex-col gap-6 md:gap-4">
+        <div>
+          <h1 className="font-semibold">Danish</h1>
+        </div>
         <div>
           <p className="text-muted-foreground font-medium">I&apos;m a <HighlightedText from="left" delay={0.4}>mechatronics</HighlightedText> engineer integrating hardware and software to create applications in machine learning, robotics, and finance. Currently taking companies from <HighlightedText delay={0.8} from="top">0 to 1</HighlightedText></p>
         </div>
--- components/copy-button.tsx
+++ components/copy-button.tsx
@@ -50,7 +50,7 @@ const CopyButton = React.forwardRef<HTMLButtonElement, CopyButtonProps>(
         aria-label={copied ? "Copied" : "Copy to clipboard"}
         disabled={copied}
         className={cn(
-          "relative cursor-pointer active:scale-[0.97] transition-all ease-out duration-200 inline-flex items-center justify-center rounded-md text-neutral-900 disabled:pointer-events-none disabled:opacity-100 dark:text-neutral-50",
+          "relative cursor-pointer active:scale-[0.97] hover:will-change-transform transition-all ease-out duration-200 inline-flex items-center justify-center rounded-md text-neutral-900 disabled:pointer-events-none disabled:opacity-100 dark:text-neutral-50",
           buttonSize,
           className,
         )}
