b80ec155adeb277fa9825d883a8782c1458dc646
Danish Ansari <aansaridan@gmail.com>
2026-07-10 12:36:29 +0300

    Try

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = { variable: "--font-geist-sans" }; /* 
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+   
+ */
 
-const geistMono = Geist_Mono({
+const geistMono = { variable: "--font-geist-mono" }; /* 
   variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+   
+ */
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://danishansari.co"),
